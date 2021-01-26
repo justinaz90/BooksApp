3981ce81cf39f3fc5559912feec971cbffc8b3a1
Justyna Szewczyk <justyna0829@gmail.com>
2021-01-26 12:38:39 +0100

    Add books to favorite

--- src/js/script.js
+++ src/js/script.js
@@ -2,29 +2,41 @@
   'use strictt';
 
   const select = {
-    templateOf: {
-      bookTemplate: '#template-book',
-    },
-    containerOf: {
-      booksList: '.books-list',
-
-    },
+    bookTemplate: '#template-book',
+    booksList: '.books-list',
+    favoriteBook: 'favorite',
+    bookImage: '.book__image',
   };
 
   const templates = {
-    templateBook: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
+    templateBook: Handlebars.compile(document.querySelector(select.bookTemplate).innerHTML),
   };
+  const bookListWrapper = document.querySelector(select.booksList);
+
+  const favoriteBooks = [];
 
   const render = function(){
-    //const thisList = this;
 
     for(let book of dataSource.books){
       const generatedHTML = templates.templateBook(book);
       const element = utils.createDOMFromHTML(generatedHTML);
-      const booksContainer = document.querySelector(select.containerOf.booksList);
+      const booksContainer = document.querySelector(select.booksList);
       booksContainer.appendChild(element);
     }
   };
-  render();
 
+  const initActions = function(){
+    const bookImages = bookListWrapper.querySelectorAll(select.bookImage);
+
+    for(let image of bookImages){
+      image.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        image.classList.add(select.favoriteBook);
+        const bookId = image.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+      });
+    }
+  };
+  render();
+  initActions();
 }
