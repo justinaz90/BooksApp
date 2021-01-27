a672d1dfcc9ecb12e688527ad236f3814009ff47
Justyna Szewczyk <justyna0829@gmail.com>
2021-01-27 18:16:24 +0100

    Add filters

--- src/js/script.js
+++ src/js/script.js
@@ -6,14 +6,19 @@
     booksList: '.books-list',
     favoriteBook: 'favorite',
     bookImage: 'book__image',
+    bookFilters: '.filters',
+    hiddenBooks: 'hidden',
   };
 
   const templates = {
     templateBook: Handlebars.compile(document.querySelector(select.bookTemplate).innerHTML),
   };
   const bookListWrapper = document.querySelector(select.booksList);
+  const bookFiltersWrapper = document.querySelector(select.bookFilters);
 
   const favoriteBooks = [];
+  const filters = [];
+
 
   const render = function(){
 
@@ -39,12 +44,48 @@
           favoriteBooks.push(bookId);
           clickedElement.classList.add(select.favoriteBook);
         } else {
-          favoriteBooks.splice(favoriteBooks.indexOf(bookId));
+          favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1);
           clickedElement.classList.remove(select.favoriteBook);
         }
       }
     });
+
+    bookFiltersWrapper.addEventListener('click', function(event){
+      const clickedElement = event.target;
+      if(clickedElement.tagName === 'INPUT' && clickedElement.type === 'checkbox' && clickedElement.name === 'filter'){
+        if(clickedElement.checked){
+          filters.push(clickedElement.value);
+          console.log('value:', clickedElement.value);
+          filterBooks();
+        } else {
+          filters.splice(filters.indexOf(clickedElement.value), 1);
+          filterBooks();
+        }
+        console.log('filters:', filters);
+      }
+    });
+
   };
+
+  const filterBooks = function(){
+    for(const book of dataSource.books){
+      let shouldBeHidden = false;
+      const toBeHidden = document.querySelector('.book__image[data-id="' + book.id + '"]');
+
+      for(const filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      if(shouldBeHidden){
+        toBeHidden.classList.add(select.hiddenBooks);
+      } else {
+        toBeHidden.classList.remove(select.hiddenBooks);
+      }
+    }
+  };
+
   render();
   initActions();
 }
